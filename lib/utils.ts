5b576aef84a89f6f975dac3e24f1c93b41aa9f9a
Sherif Ogoussan <sherif.b.j.ogoussan@gmail.com>
2024-07-31 00:32:22 +0200

    feat: added neo4j db connection

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export const nanoid = customAlphabet(
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
   7
-) // 7-character random string
+)
 
 export async function fetcher<JSON = any>(
   input: RequestInfo,
