ef4147c9ef7baa2d6e3af1213ed3c087ec89354c
Sherif <sherif.b.j.ogoussan@gmail.com>
2024-10-03 21:25:24 +0200

    style: fix flexbox in user settings

--- app/handler/layout.tsx
+++ app/handler/layout.tsx
@@ -7,7 +7,7 @@ interface ChatLayoutProps {
 
 export default async function AuthLayout({ children }: ChatLayoutProps) {
   return (
-     <ContentContainer containerClassName='flex flex-1 w-screen justify-center'>
+     <ContentContainer containerClassName='flex flex-1 w-screen justify-center [&>*]:flex-1'>
         {children}
     </ContentContainer>
   )
